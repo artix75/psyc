aac2e448442974f4fe5561b02cebf61ad77b1d3e
artix <artix2@gmail.com>
2022-12-07 22:36:55 +0100

    Remove unuseful warn for AVX test in ARM64

--- src/avx.mk
+++ src/avx.mk
@@ -1,5 +1,5 @@
 ifndef AVX
-        AVX_DEF=$(shell $(CC) -mavx2 -dM -E - < /dev/null | egrep "AVX2" | sort)
+        AVX_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx2 -dM -E - < /dev/null | egrep "AVX2" | sort)
 ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
         AVX=on
 endif
