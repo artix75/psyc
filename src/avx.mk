16c79a80c83b819b556420ef4a03c32849672ca8
artix <artix2@gmail.com>
2023-01-02 16:11:46 +0100

    Add support for AVX512 (build only)

--- src/avx.mk
+++ src/avx.mk
@@ -2,6 +2,10 @@ ifndef AVX
         AVX_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx2 -dM -E - < /dev/null | egrep "AVX2" | sort)
 ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
         AVX=on
+        AVX513_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx512f -dM -E - < /dev/null | egrep "AVX512" | sort)
+ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
+        HAS_AVX512=true
+endif
 endif
 endif
 
