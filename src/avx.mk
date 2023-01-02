faca53525f9158b6edde3dff55c232be3991a023
artix <artix2@gmail.com>
2023-01-02 16:33:11 +0100

    Temporarily disable AVX512

--- src/avx.mk
+++ src/avx.mk
@@ -2,10 +2,11 @@ ifndef AVX
         AVX_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx2 -dM -E - < /dev/null | egrep "AVX2" | sort)
 ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
         AVX=on
-        AVX513_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx512f -dM -E - < /dev/null | egrep "AVX512" | sort)
-ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
-        HAS_AVX512=true
-endif
+### AVX512 Disabled ###
+#        AVX513_DEF=$(shell $(CC) -Wno-unused-command-line-argument -mavx512f -dM -E - < /dev/null | egrep "AVX512" | sort)
+#ifeq ($(findstring AVX2,$(AVX_DEF)),AVX2)
+#        HAS_AVX512=true
+#endif
 endif
 endif
 
